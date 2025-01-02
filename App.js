abedcb0b57bba349d02244e875a4eb101747ee6f
Bassel Manasrah <bassel.manasrah@gmail.com>
2025-01-02 14:21:16 +0200

    add notification handling

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import { Text } from "react-native";
 import StackNavigator from "./src/navigation/StackNavigator";
 import { NavigationContainer } from "@react-navigation/native";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
 
 export default function App() {
   return (
