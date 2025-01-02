c1bf449820359b9f4e0cb0cc5b44b6f9fa8ecf42
Bassel Manasrah <bassel.manasrah@gmail.com>
2025-01-02 14:23:51 +0200

    add loading handling to gallery component

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,26 +1,36 @@
 import { Image, FlatList, Pressable, Button } from "react-native";
 import { useState } from "react";
+import { ActivityIndicator } from "react-native-paper";
+import ImageWrapper from "./ImageWrapper";
 // import AwesomeGallery from "react-native-awesome-gallery";
 
 const numColumns = 3;
 const spacing = 8;
 
-export default function Gallery({ images, onLongPress }) {
+export default function Gallery({ images, onLongPress, loading }) {
   const renderItem = ({ item }) => {
     return (
       <Pressable
         style={{ aspectRatio: 1, flex: 1 / numColumns, margin: spacing / 2 }}
         onLongPress={onLongPress}
       >
-        <Image
+        {/* <Image
           source={{ uri: item }}
           style={{ height: "100%", width: "100%" }}
-        />
+        /> */}
+        <ImageWrapper uri={item} />
       </Pressable>
     );
   };
 
+  if (loading) return <ActivityIndicator color="royalblue" />;
+
   return (
-    <FlatList data={images} renderItem={renderItem} numColumns={numColumns} />
+    <FlatList
+      data={images}
+      renderItem={renderItem}
+      numColumns={numColumns}
+      // contentContainerStyle={{ backgroundColor: "lightblue" }}
+    />
   );
 }
