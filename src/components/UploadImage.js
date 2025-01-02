b06bd5d79901c8c2086a9972219689c935e96f7f
Bassel Manasrah <bassel.manasrah@gmail.com>
2025-01-02 14:27:03 +0200

    add upload pdf functionality

--- src/components/UploadImage.js
+++ src/components/UploadImage.js
@@ -2,6 +2,7 @@ import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import * as ImagePicker from "expo-image-picker";
+import * as DocumentPicker from "expo-document-picker";
 
 export default function UploadImage({ onUpload }) {
   const pickImage = async () => {
@@ -10,22 +11,55 @@ export default function UploadImage({ onUpload }) {
       mediaTypes: ["images"],
       aspect: [1, 1],
       quality: 1,
-      allowsMultipleSelection: true,
+      // allowsMultipleSelection: true,
     });
 
     if (!result.canceled) {
-      const uris = result.assets.map((asset) => asset.uri);
-      onUpload(uris);
+      const uri = result.assets[0].uri; // Pick the first selected image's URI
+      onUpload(uri); // Send the single image URI to the onUpload function
+    }
+  };
+
+  const pickPDF = async () => {
+    try {
+      const result = await DocumentPicker.getDocumentAsync({
+        type: "application/pdf", // Limit to PDF files
+        copyToCacheDirectory: true, // Optionally cache the file
+      });
+
+      if (!result.canceled) {
+        const file = result.assets[0];
+        console.log("File URI:", file.uri); // Handle the selected file
+        onUpload(file.uri);
+      } else {
+        console.log("User cancelled document picker");
+      }
+    } catch (error) {
+      console.error("Error picking document:", error);
+      Alert.alert("Error", "Unable to pick document. Please try again.");
     }
   };
 
   return (
-    <TouchableOpacity onPress={pickImage}>
-      <View style={styles.container}>
-        <Ionicons name="cloud-upload-outline" size={24} />
-        <Text style={styles.title}>Upload Image</Text>
-      </View>
-    </TouchableOpacity>
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-around",
+      }}
+    >
+      <TouchableOpacity onPress={pickImage}>
+        <View style={styles.container}>
+          <Ionicons name="image-outline" size={24} />
+          <Text style={styles.title}>Upload Image</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={pickPDF}>
+        <View style={styles.container}>
+          <Ionicons name="document-outline" size={24} />
+          <Text style={styles.title}>Upload PDF</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
   );
 }
 
@@ -34,7 +68,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     borderColor: "black",
     borderWidth: 2,
-    padding: 32,
+    padding: 16,
     borderStyle: "dashed",
     backgroundColor: "#C0E863",
     justifyContent: "center",
