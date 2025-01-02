9f422b4012214ff74b0499e3b6d40c1a3602ec9c
Bassel Manasrah <bassel.manasrah@gmail.com>
2025-01-02 14:28:57 +0200

    implement auth flow in navigator

--- src/navigation/StackNavigator.js
+++ src/navigation/StackNavigator.js
@@ -1,42 +1,70 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "../screens/LoginScreen";
 import TransportsScreen from "../screens/TransportsScreen";
 import ContainersScreen from "../screens/ContainersScreen";
 import ContainerDetailsScreen from "../screens/ContainerDetailsScreen";
 import CreateRecipientScreen from "../screens/CreateRecipientScreen";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Stack = createNativeStackNavigator();
 
 export default function StackNavigator() {
+  const [driverId, setDriverId] = useState(null);
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        const id = await AsyncStorage.getItem("driverId");
+        console.log(`driverId: ${id}`);
+
+        if (id) {
+          setDriverId(id);
+        }
+      } catch (error) {
+        console.error("Error checking login status:", error);
+        setSignedIn(false); // Default to Login screen on error
+      }
+    };
+
+    checkLoginStatus();
+  }, []);
+
   return (
     <Stack.Navigator>
-      <Stack.Screen
-        name="Login"
-        component={LoginScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="Transports"
-        component={TransportsScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="Containers"
-        component={ContainersScreen}
-        options={{ headerShown: false, animation: "slide_from_right" }}
-      />
-      <Stack.Screen
-        name="ContainerDetails"
-        component={ContainerDetailsScreen}
-        options={{ headerShown: false, animation: "slide_from_right" }}
-      />
-      <Stack.Screen
-        name="CreateRecipient"
-        component={CreateRecipientScreen}
-        options={{ headerShown: false, animation: "slide_from_right" }}
-      />
+      {driverId ? (
+        <>
+          <Stack.Screen
+            name="Transports"
+            component={TransportsScreen}
+            options={{ headerShown: false }}
+            initialParams={{ driver: { id: driverId } }}
+          />
+          <Stack.Screen
+            name="Containers"
+            component={ContainersScreen}
+            options={{ headerShown: false, animation: "slide_from_right" }}
+          />
+          <Stack.Screen
+            name="ContainerDetails"
+            component={ContainerDetailsScreen}
+            options={{ headerShown: false, animation: "slide_from_right" }}
+          />
+          <Stack.Screen
+            name="CreateRecipient"
+            component={CreateRecipientScreen}
+            options={{ headerShown: false, animation: "slide_from_right" }}
+          />
+        </>
+      ) : (
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+          initialParams={{ setDriverId }}
+        />
+      )}
     </Stack.Navigator>
   );
 }
