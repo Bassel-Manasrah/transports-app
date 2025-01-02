9d1d34f619b0913973f12bcce9599dedefbed0a3
Bassel Manasrah <bassel.manasrah@gmail.com>
2025-01-02 14:25:03 +0200

    make the App component the entry component of the app

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ import TransportsScreen from "./src/screens/TransportsScreen";
 import ContainersScreen from "./src/screens/ContainersScreen";
 import ContainerDetailsScreen from "./src/screens/ContainerDetailsScreen";
 import CreateRecipientScreen from "./src/screens/CreateRecipientScreen";
+import Test from "./src/screens/Test";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 
-// registerRootComponent(App);
-registerRootComponent(ContainerDetailsScreen);
+// registerRootComponent(Test);
+
+registerRootComponent(App);
 // registerRootComponent(CreateRecipientScreen);
 // registerRootComponent(CreateRecipientScreen);
+
+// registerRootComponent(Test);
