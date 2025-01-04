024296a9979042a8aa03980362c4d54d51d6613a
Bassel Manasrah <bassel.manasrah@gmail.com>
2025-01-04 20:03:52 +0200

    fix register notification (use firebase react native instead of expo push tokens)

--- src/services/notificationService.js
+++ src/services/notificationService.js
@@ -3,49 +3,71 @@ import * as Device from "expo-device";
 import Constants from "expo-constants";
 import { Platform } from "react-native";
 import axios from "axios";
+import messaging, { getMessaging } from "@react-native-firebase/messaging";
+
+// async function registerForPushNotificationsAsync(driverId) {
+//   if (Platform.OS === "android") {
+//     Notifications.setNotificationChannelAsync("default", {
+//       name: "default",
+//       importance: Notifications.AndroidImportance.MAX,
+//       vibrationPattern: [0, 250, 250, 250],
+//       lightColor: "#FF231F7C",
+//     });
+//   }
+
+//   const { status: existingStatus } = await Notifications.getPermissionsAsync();
+//   let finalStatus = existingStatus;
+//   if (existingStatus !== "granted") {
+//     const { status } = await Notifications.requestPermissionsAsync();
+//     finalStatus = status;
+//   }
+//   if (finalStatus !== "granted") {
+//     handleRegistrationError(
+//       "Permission not granted to get push token for push notification!"
+//     );
+//     return;
+//   }
+
+//   const projectId =
+//     Constants?.expoConfig?.extra?.eas?.projectId ??
+//     Constants?.easConfig?.projectId;
+//   if (!projectId) {
+//     handleRegistrationError("Project ID not found");
+//   }
+//   try {
+//     let pushTokenString = await Notifications.getExpoPushTokenAsync({
+//       projectId,
+//     });
+
+//     pushTokenString = pushTokenString.data;
+//     console.log(pushTokenString);
+
+//     updateFcmToken(driverId, pushTokenString);
+
+//     return pushTokenString;
+//   } catch (e) {
+//     handleRegistrationError(`${e}`);
+//   }
+// }
 
 async function registerForPushNotificationsAsync(driverId) {
-  if (Platform.OS === "android") {
-    Notifications.setNotificationChannelAsync("default", {
-      name: "default",
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: "#FF231F7C",
-    });
-  }
+  console.log("Registering for push notifications");
 
-  const { status: existingStatus } = await Notifications.getPermissionsAsync();
-  let finalStatus = existingStatus;
-  if (existingStatus !== "granted") {
-    const { status } = await Notifications.requestPermissionsAsync();
-    finalStatus = status;
-  }
-  if (finalStatus !== "granted") {
-    handleRegistrationError(
-      "Permission not granted to get push token for push notification!"
-    );
-    return;
-  }
+  const authStatus = await getMessaging().requestPermission();
 
-  const projectId =
-    Constants?.expoConfig?.extra?.eas?.projectId ??
-    Constants?.easConfig?.projectId;
-  if (!projectId) {
-    handleRegistrationError("Project ID not found");
-  }
-  try {
-    let pushTokenString = await Notifications.getExpoPushTokenAsync({
-      projectId,
-    });
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
 
-    pushTokenString = pushTokenString.data;
-    console.log(pushTokenString);
-
-    updateFcmToken(driverId, pushTokenString);
-
-    return pushTokenString;
-  } catch (e) {
-    handleRegistrationError(`${e}`);
+  if (enabled) {
+    const token = await messaging().getToken();
+    if (token) {
+      console.log("FCM Token:", token);
+      return token;
+      // Send the token to your server or save it as needed
+    } else {
+      console.log("Failed to get FCM token");
+    }
   }
 }
 
