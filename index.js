16ea18c002fe72e9edc3bff61709638b04dde559
Bassel Manasrah <bassel.manasrah@gmail.com>
2025-01-04 20:03:00 +0200

    implement notification handling using expo notifications

--- index.js
+++ index.js
@@ -6,6 +6,45 @@ import ContainersScreen from "./src/screens/ContainersScreen";
 import ContainerDetailsScreen from "./src/screens/ContainerDetailsScreen";
 import CreateRecipientScreen from "./src/screens/CreateRecipientScreen";
 import Test from "./src/screens/Test";
+import messaging from "@react-native-firebase/messaging";
+import notifee from "@notifee/react-native";
+import * as Notifications from "expo-notifications";
+
+async function requestPermissions() {
+  const { status } = await Notifications.getPermissionsAsync();
+  if (status !== "granted") {
+    const { status: newStatus } = await Notifications.requestPermissionsAsync();
+    if (newStatus !== "granted") {
+      alert("Permission to send notifications is required!");
+    }
+  }
+}
+
+// Schedule a notification
+async function pushNotification({ title, body }) {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: title,
+      body: body,
+    },
+    trigger: { seconds: 0 }, // Trigger after 5 seconds
+  });
+}
+
+// Register background handler
+messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+  console.log("received notification:", remoteMessage);
+
+  const { status } = await Notifications.getPermissionsAsync();
+  if (status !== "granted") {
+    const { status: newStatus } = await Notifications.requestPermissionsAsync();
+    if (newStatus !== "granted") {
+      alert("Permission to send notifications is required!");
+    }
+  } else {
+    await pushNotification(remoteMessage.data);
+  }
+});
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
